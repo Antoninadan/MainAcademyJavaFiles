36c0c435c3b5a3ca55e50a1a750c7aa7a7ac2209
Antoninadan <mail100@i.ua>
2019-10-29 15:07:28 +0200

    add tests

---
+++ src/com/mainacad/model/ConnectionInfo.java
@@ -0,0 +1,45 @@
+package com.mainacad.model;
+
+public class ConnectionInfo {
+    private String ip;
+    private Integer sessionId;
+    private Long connectionTime;
+
+    public ConnectionInfo(String ip, Integer sessionId, Long connectionTime) {
+        this.ip = ip;
+        this.sessionId = sessionId;
+        this.connectionTime = connectionTime;
+    }
+
+    public ConnectionInfo() {
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Integer sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Long getConnectionTime() {
+        return connectionTime;
+    }
+
+    public void setConnectionTime(Long connectionTime) {
+        this.connectionTime = connectionTime;
+    }
+
+    @Override
+    public String toString() {
+        return sessionId + " " + connectionTime + " " + ip;
+    }
+}
